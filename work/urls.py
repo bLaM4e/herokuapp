7f5b0bd981efcb55921ad134b1b25029b81e7057
Dmitry <konnerspz@mail.ru>
2021-03-25 10:42:44 +0300

    add_favicon

--- work/urls.py
+++ work/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
     path('', mainapp.index),
     path('admin/', admin.site.urls),
+    path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico'), name='favicon'),
 ]
